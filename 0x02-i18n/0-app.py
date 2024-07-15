0903888d4821daeaf36e7fb29c0f7a6a32162154
alombajoseph <alombajoseph@gmail.com>
2024-07-15 18:05:02 +0000

    0x02-i18n

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+sic Flask app '''
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world() -> str:
+    ''' Output templates '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
